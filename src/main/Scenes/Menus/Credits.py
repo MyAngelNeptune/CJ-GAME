227047fe02f6fd9f27a77cc1351b069866d798b5
Naranja-Sagged <alexliuying@gmail.com>
2018-03-11 00:34:37 -0800

    return credits

--- src/main/Scenes/Menus/Credits.py
+++ src/main/Scenes/Menus/Credits.py
@@ -1,4 +1,5 @@
 import pygame
+from src.main.Scenes.Button import Button
 class Credits(object):
     def __init__(self):
         super(Credits, self).__init__()
@@ -7,6 +8,9 @@ class Credits(object):
         self.image = pygame.image.load("Assets/Screens/creditpage.png")
         self.image = pygame.transform.scale(self.image, (1200, 600))
         screen.blit(self.image, [0, 0])
+
+        returnButton = Button("Assets/Buttons/return.png", (600.727, 727-300), (300, 95))
+        returnButton.draw(screen)
 
     def update(self):
         pass
